18cef47101433cef6e10959f839c56eba20b7f55
R.Berk Karatas <rberkkaratas@gmail.com>
2024-05-31 14:29:37 +0300

    added & fixed: dark mode initialized & navbar refactoring

--- src/components/LanguageChanger.jsx
+++ src/components/LanguageChanger.jsx
@@ -3,26 +3,19 @@ import { useTranslation } from 'react-i18next';
 
 export default function LanguageChanger() {
     const { i18n } = useTranslation()
+    const toggleLanguage = () => {
+        const newLanguage = i18n.language === 'en' ? 'tr' : 'en';
+        i18n.changeLanguage(newLanguage);
+    };
 
-    const lngs = {
-        en: { nativeName: 'EN' },
-        tr: { nativeName: 'TR' },
-    }
     return (
-        <div className="inline-flex rounded-md shadow-sm" role="group">
-            {Object.keys(lngs).map((lng) => (
-                <button
-                    type='submit'
-                    key={lng}
-                    onClick={() => i18n.changeLanguage(lng)}
-                    disabled={i18n.resolvedLanguage === lng}
-                    className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
-                >
-                    {lngs[lng].nativeName}
-                </button>
-
-            ))}
-            
+        <div className="flex justify-center ">
+            <div
+                onClick={toggleLanguage}
+                className="px-4 py-2 text-2xl font-medium text-gray-900 bg-white focus:outline-none focus:ring-2  dark:bg-gray-900  dark:text-gray-300  "
+            >
+                {i18n.language === 'en' ? '🇹🇷' : '🇬🇧'}
+            </div>
         </div>
-    )
+    );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./utils/i18n"
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

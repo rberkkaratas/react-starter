1056f89acb8404102cddd0619e7e1a2be012c4b4
R.Berk Karatas <rberkkaratas@gmail.com>
2024-05-31 13:51:25 +0300

    added: tailwind added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./container/Home";
 import About from "./container/About";
+import "./index.css"
 
 function App() {
   return (
